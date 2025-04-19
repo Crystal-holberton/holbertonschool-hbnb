467ae10251b8ee1a9b62e475be8a8036c01d1dda
Crystal-holberton <9188@holbertonstudents.com>
2025-04-19 17:10:52 +1000

    It works!

--- part-04/vite.config.js
+++ part-04/vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling:true,
+    },
+  },
 })
